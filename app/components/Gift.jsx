f4b912a799d4b0f6e5a76937a7cd66b7f29caea4
douglas <douglimaonline@gmail.com>
2025-02-02 18:10:07 -0300

    Add Gift Component

---
+++ app/components/Gift.jsx
@@ -0,0 +1,12 @@
+import styles from '@/app/styles/gift.module.css'
+
+export default function Gift() {
+  return (
+    <div className={styles.gift}>
+      <div className={styles.top}></div>
+      <div className={styles.body}></div>
+      <div className={styles.bannerY}></div>
+      <div className={styles.bannerX}></div>
+    </div>
+  )
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
+import Gift from '@/components/Gift'
+
 export default function Home() {
   return (
-    <>
+    <div>
       <div>Home</div>
-    </>
+      <Gift />
+      <Gift />
+    </div>
   )
 }
