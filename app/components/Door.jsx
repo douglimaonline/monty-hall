3cd23f14fdde1b2268f127509957958423722df0
douglas <douglimaonline@gmail.com>
2025-02-03 19:16:08 -0300

    feature: Improve door css

--- app/components/Door.jsx
+++ app/components/Door.jsx
@@ -2,9 +2,12 @@ import styles from '@/app/styles/door.module.css'
 
 export default function Door() {
   return (
-    <div className={styles.door}>
-      <div className={styles.number}>3</div>
-      <div className={styles.knob}></div>
+    <div className={styles.area}>
+      <div className={styles.door}>
+        <div className={styles.number}>3</div>
+        <div className={styles.knob}></div>
+      </div>
+      <div className={styles.floor}></div>
     </div>
   )
 }
