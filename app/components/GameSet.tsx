ce764e2d2468a9cf1094b222f9172aab8d76d974
douglas <douglimaonline@gmail.com>
2025-02-18 21:06:06 -0300

    Delete unused imports

--- app/components/GameSet.tsx
+++ app/components/GameSet.tsx
@@ -1,9 +1,3 @@
-import { useState } from 'react'
-
-interface GameSetProps {
-  onChange: (totalDoors: number, selectedDoor: number) => void
-}
-
 export default function GameSet() {
   return <h1>Game Set</h1>
 }
