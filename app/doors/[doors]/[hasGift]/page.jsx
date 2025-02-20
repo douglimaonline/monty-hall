fdd73e9b5444eb6d64b12f729193e61d9b899e1a
douglas <douglimaonline@gmail.com>
2025-02-19 22:27:19 -0300

    Implement custom path

---
+++ app/doors/[doors]/[hasGift]/page.jsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import Door from '../../../components/Door'
+import styles from '../../../styles/door.module.css'
+import {
+  createDoors,
+  handleDoorChange,
+  getCreateDoorProps,
+} from '../../../utils/doorsFunctions'
+
+export default function Doors() {
+  const pathName = usePathname()
+  const [doors, setDoors] = useState([])
+
+  useEffect(() => {
+    const [doors, hasGift] = getCreateDoorProps(pathName)
+    setDoors(createDoors(doors, hasGift))
+  }, [pathName])
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.doors}>
+        {doors.map((d) => {
+          return (
+            <Door
+              key={d?.number}
+              door={d}
+              onChange={(e) => handleDoorChange(e, setDoors)}
+            />
+          )
+        })}
+      </div>
+      <div style={{ alignSelf: 'center' }}>
+        <Link href="/">
+          <button className={styles.button}>Restart</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
--- app/utils/doorsFunctions.ts
+++ app/utils/doorsFunctions.ts
@@ -1,14 +1,14 @@
 import DoorModel from '../models/Door'
 
-export function createDoors(qnt: number, selectedDoor?: number): DoorModel[] {
+export function createDoors(qnt: number, hasGiftDoor?: number): DoorModel[] {
   return Array.from({ length: qnt }, (_, i) => {
     const num = i + 1
-    const hasGift = num === selectedDoor
+    const hasGift = num === Number(hasGiftDoor)
     return new DoorModel(num, hasGift)
   })
 }
 
-export const handleDoorChange = (updatedDoor, setDoors) => {
+export function handleDoorChange(updatedDoor, setDoors) {
   setDoors((prevDoors) => {
     return prevDoors.map((door) => {
       return door.number === updatedDoor.number
@@ -18,4 +18,11 @@ export const handleDoorChange = (updatedDoor, setDoors) => {
         : door.unSelect()
     })
   })
+}
+
+export function getCreateDoorProps(url: string): number[] {
+  const createDoorProps: number[] = []
+  createDoorProps.push(+url.split('/')[2])
+  createDoorProps.push(+url.split('/')[3])
+  return createDoorProps
 }
